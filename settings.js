672dc6210468afbe33cf11bfb10934f9ee60a63a
xwby <160570513+xwbyy@users.noreply.github.com>
2024-06-25 14:01:28 +0700

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,37 @@
+const previews = [
+    {
+        url: "https://vynaavalerie.github.io/BioLink1/",
+        price: "Rp 40.000",
+    },
+    {
+        url: "https://vynaavalerie.github.io/BioLink2/",
+        price: "Rp 60.000",
+    },
+    {
+        url: "https://vynaavalerie.github.io/BioLink3",
+        price: "Rp 40.000",
+    },
+    {
+        url: "https://hunter-ivory.vercel.app/",
+        price: "Rp 50.000",
+    },
+    {
+        url: "https://www.dyonns.shop",
+        price: "Rp 50.000",
+    },
+    {
+        url: "https://www.vynaachan-api.shop",
+        price: "Rp 60.000",
+    }
+];
+
+const domains = [
+    { name: ".online", price: "IDR 40.000" },
+    { name: ".shop", price: "IDR 55.000" },
+    { name: ".store", price: "IDR 60.000" },
+    { name: ".tech", price: "IDR 50.000" },
+    { name: ".cloud", price: "IDR 35.000" },
+    { name: ".site", price: "IDR 40.000" },
+    { name: ".my.id", price: "IDR 40.000" },
+    { name: ".fun", price: "IDR 35.000" }
+];
